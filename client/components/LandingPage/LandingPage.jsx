37250e2af9a1124e422bc8dbffa56d2295acb0f3
Mark Fuechec <fuechec.mark@gmail.com>
2020-05-03 10:00:22 -0500

    Finished Landing Page

--- client/components/LandingPage/LandingPage.jsx
+++ client/components/LandingPage/LandingPage.jsx
@@ -5,10 +5,10 @@ let LandingPage = (props) => {
     return (
         <div id='LandingPageOptionsContainer'>
             <Link to='/NewFood'>
-                <div id='LandingPageNewFoodOption' />
+                <img src={'https://i.pinimg.com/originals/ec/c7/a4/ecc7a4f021aab13bf5197543d291adf7.jpg'} id='LandingPageNewFoodOption' />
             </Link>
             <Link to='/NewDrinks'>
-                <div id='LandingPageNewDrinksOption' />
+                <img src={'https://cache.desktopnexus.com/thumbseg/2381/2381921-bigthumbnail.jpg'} id='LandingPageNewDrinksOption' />
             </Link>
         </div>
     )
