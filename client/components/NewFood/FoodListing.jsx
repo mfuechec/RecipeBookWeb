317402f9bc034dc70d67232962ccc5782715c071
Mark Fuechec <fuechec.mark@gmail.com>
2020-05-03 14:39:29 -0500

    Food Listing well spaced

--- client/components/NewFood/FoodListing.jsx
+++ client/components/NewFood/FoodListing.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import IngredientsList from './IngredientsList.jsx';
 
 let FoodListing = (props) => {
     let food = props.food;
     return (
         <div id='FoodListingContainer'>
             <Link to='/Food' onClick={() => props.setFood(food)}>
-                {props.food.name}
+                <div id='FoodListingContainerView'>
+                    <div id='FoodListingImageContainer'>
+                        <img id='FoodListingImage' src={food.image}/>
+                    </div>
+                    <div id='FoodListingNameContainer'>
+                        <p id='FoodListingNameText'>{food.name}</p>
+                        <IngredientsList ingredients={food.ingredients}/>
+                    </div>
+                    <div id='FoodListingFavContainer'>
+                        <p id='FoodListingFavText'>Favorite</p>
+                    </div>
+                </div>
             </Link>
         </div>
     )
---
+++ client/components/NewFood/IngredientsList.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+let IngredientsList = (props) => {
+    return (
+        <div>{props.ingredients[0]}</div>
+    )
+}
+
+export default IngredientsList;
