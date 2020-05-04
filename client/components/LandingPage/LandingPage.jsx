829c68be45e9332123ac6a5345bcee44517a933e
Mark Fuechec <fuechec.mark@gmail.com>
2020-05-04 13:55:43 -0500

    Changed LandingPage styling to better fit iPhone 6/7/8, wrapped all styling in iPhone 6/7/8 media query

--- client/components/LandingPage/LandingPage.jsx
+++ client/components/LandingPage/LandingPage.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 let LandingPage = (props) => {
     return (
         <div id='LandingPageOptionsContainer'>
-            <Link to='/NewFood'>
+            <Link id='LandingPageLinks' to='/NewFood'>
                 <img src={'https://recipe-book-images.s3.us-east-2.amazonaws.com/images/foodBackground.jpg'} id='LandingPageNewFoodOption' />
             </Link>
-            <Link to='/NewDrinks'>
+            <Link id='LandingPageLinks' to='/NewDrinks'>
                 <img src={'https://recipe-book-images.s3.us-east-2.amazonaws.com/images/drinksBackground.jpg'} id='LandingPageNewDrinksOption' />
             </Link>
         </div>
