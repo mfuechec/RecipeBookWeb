36c6dfaae1d0fb77addcf66715e4db5defdd89a2
Mark Fuechec <fuechec.mark@gmail.com>
2020-05-04 12:23:02 -0500

    Moved FoodDirections and DrinkDirections to seperate folders, bundled for production

---
+++ client/components/FoodDirections/FoodDirections.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const FoodDirections = props => {
+    return (
+        <div id='FoodDirectionsContainer'>
+            <div>{props.food.name}</div>
+            <div>{props.food.type}</div>
+            <div>{props.food.instructions}</div>
+            <div>{props.food.image}</div>
+            <div>{props.food.ingredients}</div>
+            <div>{props.food.measurements}</div>
+        </div>
+    )
+}
+
+export default FoodDirections;
