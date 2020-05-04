d0dad99aa571c08a90bc6de0549c1ede5bbc4a72
Mark Fuechec <fuechec.mark@gmail.com>
2020-05-04 00:21:16 -0500

    A bit of experimenting in server.js

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 let express = require('express');
 let cors = require('cors');
-let app = express();
 let port = process.env.PORT || 4001;
+let app = express();
 
 app.use(cors());
-app.use('/', express.static('public'));
-app.get('/', express.static('public'));
 
-app.use('/bundle', express.static('public/bundle.ts'));
-app.use('/styleSheet', express.static('public/styles.css'));
+app.use('/', express.static('public'));
+app.use('/bundle', express.static('public/bundle'));
+app.use('/styleSheet', express.static('public/styles'));
 
 app.listen(port, () => {
     console.log(`Server listening at ${port}`);
