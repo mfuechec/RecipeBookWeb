9ef175b956192c54de03160ddc986c3f82bfbb56
Mark Fuechec <fuechec.mark@gmail.com>
2020-05-05 11:38:41 -0500

    Changed fav button to star icon, restyled listing page landscape orientation

--- client/components/NewDrinks/DrinkListing.jsx
+++ client/components/NewDrinks/DrinkListing.jsx
@@ -10,14 +10,18 @@ let DrinkListing = (props) => {
                 <Link id='ListingImageContainer' to='/Drink' onClick={() => props.setDrink(drink)}>
                     <img id='ListingImage' src={drink.image} />
                 </Link>
-                <Link id='ListingNameContainer' to='/Drink' onClick={() => props.setDrink(drink)}>
-                    <div id='ListingNameText'>{drink.name}</div>
-                    <div id='ListingIngredientsList'>
-                        <IngredientsList ingredients={drink.ingredients} />
+                <div id='ListingDetailsContainer'>
+                    <Link id='ListingNameContainer' to='/Drink' onClick={() => props.setDrink(drink)}>
+                        <div id='ListingNameText'>{drink.name}</div>
+                    </Link>
+                    <div id='ListingIngredientsAndFavContainer'>
+                        <Link id='ListingIngredientsList' to='/Drink' onClick={() => props.setDrink(drink)}>
+                            <IngredientsList ingredients={drink.ingredients} />
+                        </Link>
+                        <div id='ListingFavContainer' onClick={() => { props.manageAPICalls.editFavorites(drink, 'POST') }}>
+                            <i class="fas fa-star"></i>
+                        </div>
                     </div>
-                </Link>
-                <div id='ListingFavContainer' onClick={() => { props.manageAPICalls.editFavorites(drink, 'POST') }}>
-                    <p id='ListingFavText'>Favorite</p>
                 </div>
             </div>
         </div>
