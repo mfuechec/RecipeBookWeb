4dc3da7c13a4314eaf312d5aa7d3c34e805c7a5c
Mark Fuechec <fuechec.mark@gmail.com>
2020-05-09 13:47:06 -0500

    Added Header to Listing page

--- client/components/NewDrinks/NewDrinks.jsx
+++ client/components/NewDrinks/NewDrinks.jsx
@@ -3,13 +3,17 @@ import DrinksList from './DrinksList.jsx';
 
 let NewDrink = (props) => {
     let drinks;
+    let text;
     if (props.lookingAtFavorites === false) {
         drinks = props.drinks;
+        text = 'Recipes';
     } else {
         drinks = props.favDrinks;
+        text = 'Favorites';
     }
     return (
         <div id='NewDrinkRecipesContainer'>
+            <div id='NewRecipesHeader'>{text}</div>
             <DrinksList manageAPICalls={props.manageAPICalls} setDrink={props.setDrink} drinks={drinks} />
         </div>
     )
--- client/components/NewFood/NewFood.jsx
+++ client/components/NewFood/NewFood.jsx
@@ -3,13 +3,17 @@ import FoodsList from './FoodsList.jsx';
 
 let NewFood = (props) => {
     let foods;
+    let text;
     if (props.lookingAtFavorites === false) {
         foods = props.foods;
+        text = 'Recipes';
     } else {
-        foods = props.favFoods
+        foods = props.favFoods;
+        text = 'Favorites';
     }
     return (
         <div id='NewFoodRecipesContainer'>
+            <div id='NewRecipesHeader'>{text}</div>
             <FoodsList manageAPICalls={props.manageAPICalls} setFood={props.setFood} foods={foods} />
         </div>
     )
