7447533b9a27fc7f837e823a27680679dbe894a1
Mark Fuechec <fuechec.mark@gmail.com>
2020-06-20 14:53:05 -0500

    Changed splash screen image, changed background color

--- client/components/SplashScreen/SplashScreen.jsx
+++ client/components/SplashScreen/SplashScreen.jsx
@@ -1,34 +1,7 @@
 import React, { useState, useEffect } from "react";
-import web1 from "../../../assets/SplashBackgrounds/Web-Image-1.jpg";
-import web2 from "../../../assets/SplashBackgrounds/Web-Image-2.jpg";
-import web3 from "../../../assets/SplashBackgrounds/Web-Image-3.jpg";
-import web4 from "../../../assets/SplashBackgrounds/Web-Image-4.jpg";
-import web5 from "../../../assets/SplashBackgrounds/Web-Image-5.jpg";
-import web6 from "../../../assets/SplashBackgrounds/Web-Image-6.jpg";
-
-import mobile1 from "../../../assets/SplashBackgrounds/Mobile-Image-1.jpg";
-import mobile2 from "../../../assets/SplashBackgrounds/Mobile-Image-2.jpg";
-import mobile3 from "../../../assets/SplashBackgrounds/Mobile-Image-3.jpg";
-import mobile4 from "../../../assets/SplashBackgrounds/Mobile-Image-4.jpg";
-import mobile5 from "../../../assets/SplashBackgrounds/Mobile-Image-5.jpg";
-import mobile6 from "../../../assets/SplashBackgrounds/Mobile-Image-6.jpg";
+import recipeBook from "../../../assets/SplashBackgrounds/recipes.png";
 
 let SplashScreen = (props) => {
-  const [image, setImage] = useState(web1);
-
-  useEffect(() => {
-    let numImages = 6;
-    let web = [web1, web2, web3, web4, web5, web6];
-    let mobile = [mobile1, mobile2, mobile3, mobile4, mobile5, mobile6];
-
-    let index = Math.floor(Math.random() * numImages);
-    if (window.innerWidth > window.innerHeight) {
-      setImage(web[index]);
-    } else {
-      setImage(mobile[index]);
-    }
-  }, []);
-
   useEffect(() => {
     if (props.firstImagesLoaded && props.secondLoad === false) {
       fadeOut();
@@ -38,6 +11,7 @@ let SplashScreen = (props) => {
   function fadeOut() {
     let element = document.getElementById(`splashContainer`);
     element.style.opacity = 0;
+    element.style.backgroundColor = "#86a3c3";
     setTimeout(() => {
       let element2 = document.getElementById(`splashImage`);
       element.style.zIndex = -1;
@@ -50,7 +24,7 @@ let SplashScreen = (props) => {
   } else {
     return (
       <div id="splashContainer">
-        <img id="splashImage" src={image} />
+        <img id="splashImage" src={recipeBook} />
       </div>
     );
   }
