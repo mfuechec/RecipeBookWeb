cc66b9f1070cfc590dcfc1188427d985fd46ab3f
Mark Fuechec <fuechec.mark@gmail.com>
2020-06-20 02:22:44 -0500

    Removed login close button, removed focus outline on login buttons

--- client/components/LogIn/LogIn.jsx
+++ client/components/LogIn/LogIn.jsx
@@ -27,23 +27,9 @@ let LogIn = (props) => {
     let loginContainer = document.getElementById("loginContainer");
     let loginUsername = document.getElementById("loginUsername");
     let loginPassword = document.getElementById("loginPassword");
-    let loginClose = document.getElementById("loginClose");
     loginContainer.id = "loginContainerExpanded";
     loginUsername.id = "loginUsernameExpanded";
     loginPassword.id = "loginPasswordExpanded";
-    loginClose.id = "loginCloseExpanded";
-  }
-
-  function closeLogin() {
-    let loginContainer = document.getElementById("loginContainerExpanded");
-    let loginUsername = document.getElementById("loginUsernameExpanded");
-    let loginPassword = document.getElementById("loginPasswordExpanded");
-    let loginClose = document.getElementById("loginCloseExpanded");
-    loginContainer.id = "loginContainer";
-    loginUsername.id = "loginUsername";
-    loginPassword.id = "loginPassword";
-    loginClose.id = "loginClose";
-    setFirstPress(true);
   }
 
   if (props.loggedIn === true) {
@@ -72,9 +58,6 @@ let LogIn = (props) => {
         <button id="signUpButton" onClick={signUp}>
           Sign Up
         </button>
-        <div id="loginClose" onClick={closeLogin}>
-          X
-        </div>
       </div>
     );
   }
