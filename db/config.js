f2dc88021162c308af2c33c2494a7c1470e64ced
Mark Fuechec <fuechec.mark@gmail.com>
2020-07-12 12:29:53 -0500

    Delete config.js

--- db/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  connectionLimit: 10,
-  host: "developmentdatabase.cpahaehounti.us-east-2.rds.amazonaws.com",
-  user: "mfuechec",
-  password: "Madison01",
-  database: "RecipeBook",
-  port: 3306,
-};
