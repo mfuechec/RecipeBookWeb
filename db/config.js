3d321efb7270ff525953c9a75736e1b6f881d9d4
Mark Fuechec <fuechec.mark@gmail.com>
2020-07-12 12:37:47 -0500

    Added config.js back

---
+++ db/config.js
@@ -0,0 +1,11 @@
+// let APIKeys = require('../passwords.js')
+
+module.exports = {
+    connectionLimit: 10,
+    host: "developmentdatabase.cpahaehounti.us-east-2.rds.amazonaws.com",
+    user: "mfuechec",
+    password: 'Madison01',
+    database: "RecipeBook",
+    port: 3306,
+  };
+  
